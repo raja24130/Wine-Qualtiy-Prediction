8d64f16a8952e055d66042934f05479f8fc83f0f
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 22:01:29 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scikit-learn.ensemble import RandomForestClassifier
+
 #app heading
 st.write("""
 # Wine Quality Prediction App
